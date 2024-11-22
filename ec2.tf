d342989d66f6a7826fdcacd8b56435cd2934dbd5
Nidhi Gupta <nidhi@Nidhis-MacBook-Air.local>
2024-11-22 10:41:27 -0500

    asg

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,63 @@
-data "aws_ami" "amazon_linux_3" {
-  most_recent = true
-  owners      = ["137112412989"] # Amazon's official owner ID
+resource "aws_launch_template" "nodes" {
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
 
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64-gp2"]
+      kms_key_id            = data.aws_kms_key.encryption.arn
+      delete_on_termination = true
+      volume_type           = "gp2"
+      volume_size           = "50"
+      encrypted             = true
+    }
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  name_prefix   = "${var.environment}"
+  image_id      = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t2.micro"
+  user_data = base64encode(templatefile("${path.module}/templates/user_data.tpl", {
+    ssh_public_key = var.ssh_public_key
+  }))
+  key_name = "ansible.pem"
+  network_interfaces {
+    security_groups = [aws_security_group.jobmgr_group.id]
   }
 }
 
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [module.vpc.vpc_id]
+resource "aws_autoscaling_group" "jobmgr" {
+  name                = "${var.environment}-asg"
+  desired_capacity    = "1"
+  max_size            = "1"
+  min_size            = "1"
+  vpc_zone_identifier = data.aws_subnets.private.ids
+  launch_template {
+    id      = aws_launch_template.nodes.id
+    version = aws_launch_template.nodes.latest_version
+  }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage       = 80
+    }
+  }
+  tag {
+    key                 = "Name"
+    value               = "${var.environment}-node"
+    propagate_at_launch = true
+  }
+
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${lower(var.environment)}-sg"
+  description = "Security group for EC2 Instances"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "*Private*"
+    Name = "sg-${var.environment}"
   }
 }
 
