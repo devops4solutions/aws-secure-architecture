566869e5d0217ac5c76e6d3d1300e336235314af
Nidhi Gupta <nidhi@Nidhis-MacBook-Air.local>
2024-11-22 10:43:06 -0500

    test

--- ec2.tf
+++ ec2.tf
@@ -60,18 +60,3 @@ resource "aws_security_group" "sg" {
     Name = "sg-${var.environment}"
   }
 }
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_linux_3.id
-  subnet_id = data.aws_subnets.private.id
-  instance_type = "t2.micro"
-  key_name = "ansible.pem"
-   user_data = <<-EOF
-    #!/bin/bash
-    mkdir -p /home/ec2-user/.ssh
-    echo "${var.ssh_public_key}" >> /home/ec2-user/.ssh/authorized_keys
-    chmod 600 /home/ec2-user/.ssh/authorized_keys
-    chmod 700 /home/ec2-user/.ssh
-    chown -R ec2-user:ec2-user /home/ec2-user/.ssh
-  EOF
-}
