612fb1846b818e72a673516e276d3c4db19a0bee
Nidhi Gupta <nidhi@Nidhis-MacBook-Air.local>
2024-11-22 10:56:31 -0500

    test

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_launch_template" "nodes" {
       //kms_key_id            = data.aws_kms_key.encryption.arn
       delete_on_termination = true
       volume_type           = "gp2"
-      volume_size           = "50"
+      volume_size           = "8"
       encrypted             = true
     }
   }
